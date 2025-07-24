395268f3e70f99bcf0b8bf526288494f589da9ba
Lucas de Oliveira Batista <lucasolibatista@gmail.com>
2025-07-23 23:00:44 -0300

    Feat: register page

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Home from './src/pages/Home'
-import Login from './src/pages/Login'
-import './src/themes/App.css'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './src/pages/Home';
+import Login from './src/pages/Login';
+import Registro from './src/pages/Registro';
+import './src/themes/App.css';
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={ <Home /> } />
         <Route path="/login" element={ <Login /> } />
+        <Route path="/registro" element={ <Registro /> } />
       </Routes>
   )
 }
